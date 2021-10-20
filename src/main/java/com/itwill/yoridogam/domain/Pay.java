1df023b43f658bad20fe870ba97a6bb06807c5a9
nufy02 <nufy02@naver.com>
2021-10-21 01:10:02 +0900

    ..

--- src/main/java/com/itwill/yoridogam/domain/Pay.java
+++ src/main/java/com/itwill/yoridogam/domain/Pay.java
@@ -1,5 +1,67 @@
 package com.itwill.yoridogam.domain;
 
-public class Pay {
+import java.sql.Date;
+import java.util.ArrayList;
 
+public class Pay {
+	private String pay_no;
+	private Date pay_date;
+	private String pay_type;
+	//private Member member;
+	private ArrayList<Pay_item> payItemList;
+	
+	public Pay() {
+		
+	}
+	/*
+	public Pay(String pay_no, Date pay_date, String pay_type, Member member, ArrayList<Pay_item> payItemList) {
+		super();
+		this.pay_no = pay_no;
+		this.pay_date = pay_date;
+		this.pay_type = pay_type;
+		this.member = member;
+		this.payItemList = payItemList;
+	}
+
+	public String getPay_no() {
+		return pay_no;
+	}
+
+	public void setPay_no(String pay_no) {
+		this.pay_no = pay_no;
+	}
+
+	public Date getPay_date() {
+		return pay_date;
+	}
+
+	public void setPay_date(Date pay_date) {
+		this.pay_date = pay_date;
+	}
+
+	public String getPay_type() {
+		return pay_type;
+	}
+
+	public void setPay_type(String pay_type) {
+		this.pay_type = pay_type;
+	}
+
+	public Member getMember() {
+		return member;
+	}
+
+	public void setMember(Member member) {
+		this.member = member;
+	}
+
+	public ArrayList<Pay_item> getPayItemList() {
+		return payItemList;
+	}
+
+	public void setPayItemList(ArrayList<Pay_item> payItemList) {
+		this.payItemList = payItemList;
+	}
+	*/
+	
 }
---
+++ src/main/java/com/itwill/yoridogam/sql/db/insert.sql
@@ -0,0 +1,37 @@
+--실데이터 용
+
+/********** Member **********/
+
+
+/********** Product **********/
+
+
+/********** Teacher **********/
+/*
+insert into teacher(t_id, t_name, t_pass, t_email, t_photo, t_detail, t_location, p_no)
+values('teacher1','홍길동','pw12','teacher1@naver.com','img/teacher1.png','강사소개글','서울시 강남구 강남대로 3길 3-3 삼조빌딩',1);
+*/
+
+/********** Product_time **********/
+
+
+/********** Cart **********/
+
+
+/********** Pay **********/
+
+
+/********** Pay_item **********/
+
+
+/********** Reservation **********/
+
+
+/********** Review **********/
+
+
+/********** Notice **********/
+
+
+/********** Inquiry **********/
+
