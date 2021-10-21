e09921a29b34017157570fe98f3bb09c95ef4218
nufy02 <nufy02@naver.com>
2021-10-21 22:20:12 +0900

    pay mapper

---
+++ src/main/java/com/itwill/yoridogam/repository/PayDaoImpl.java
@@ -0,0 +1,63 @@
+package com.itwill.yoridogam.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.itwill.yoridogam.domain.Pay;
+
+@Repository("PayDao")
+public class PayDaoImpl implements PayDao{
+	public static final String NAMESPACE="com.itwill.yoridogam.mapper.CartMapper.";
+	private SqlSession sqlSession;
+	
+	@Override
+	public int createPay(Pay pay) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deletePayByNo(int pay_no, String sUserId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deletePay(String sUserId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isExistPay(String sUserId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Pay findPayByNo(int pay_no, String sUserId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Pay> findPayList(String sUserId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Pay> findPayDetailList(String sUserId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Pay findPayDetailByNo(String sUserId, int pi_no) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
