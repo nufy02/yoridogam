482f402fd89055383e2795075f0c1bf125032106
전하라 <doit160493@gmail.com>
2021-10-21 21:08:59 +0900

    notice

--- src/main/java/com/itwill/yoridogam/repository/NoticeDao.java
+++ src/main/java/com/itwill/yoridogam/repository/NoticeDao.java
@@ -1,7 +1,25 @@
 package com.itwill.yoridogam.repository;
 
+import java.util.List;
+
+import com.itwill.yoridogam.domain.Notice;
 
 public interface NoticeDao {
 
+	/*************** 공지사항 추가 ***************/
+	int insert(Notice notice);
+	
+	/*************** 공지사항 수정 ***************/
+	int updateNoti(String noti_title, String noti_content, int noti_no);
+	
+	/*************** 공지사항 삭제 ***************/
+	int deleteNoti(int noti_no);
+	
+	/*************** 공지사항 전체 조회 ***************/
+	List<Notice> selectAllNoti();
+	
+	/*************** 공지사항 한 개 보기 ***************/
+	List<Notice> selectByNo(int noti_no);
+	
 	
 }
---
+++ src/main/java/com/itwill/yoridogam/repository/NoticeDaoImpl.java
@@ -0,0 +1,39 @@
+package com.itwill.yoridogam.repository;
+
+import java.util.List;
+
+import com.itwill.yoridogam.domain.Notice;
+
+public class NoticeDaoImpl implements NoticeDao {
+
+	@Override
+	public int insert(Notice notice) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateNoti(String noti_title, String noti_content, int noti_no) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteNoti(int noti_no) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Notice> selectAllNoti() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Notice> selectByNo(int noti_no) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
