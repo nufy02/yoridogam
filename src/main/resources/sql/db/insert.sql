0152132f2eaad198424417d9d2786ef4d18356d9
eogus8292 <eogus8292@naver.com>
2021-10-22 17:22:19 +0900

    insert SQL에 pruductTime, reservation 추가

--- src/main/resources/sql/db/insert.sql
+++ src/main/resources/sql/db/insert.sql
@@ -21,7 +21,25 @@ insert into teacher(t_id,t_name,t_pass,t_email,t_photo,t_detail,t_location,p_no)
 
 
 /********** Product_time **********/
+/*
+insert into product_time(pt_no,pt_date, pt_time, p_no) 
+values(PRODUCT_TIME_PT_NO_SEQ.nextval,to_date('2021/10/21','YYYY/MM/DD'),'10:00-11:00',?);
 
+insert into product_time(pt_no,pt_date, pt_time, p_no) 
+values(PRODUCT_TIME_PT_NO_SEQ.nextval,to_date('2021/10/21','YYYY/MM/DD'),'11:00-12:00',?);
+
+insert into product_time(pt_no,pt_date, pt_time, p_no) 
+values(PRODUCT_TIME_PT_NO_SEQ.nextval,to_date('2021/10/21','YYYY/MM/DD'),'13:00-14:00',?);
+
+insert into product_time(pt_no,pt_date, pt_time, p_no) 
+values(PRODUCT_TIME_PT_NO_SEQ.nextval,to_date('2021/10/22','YYYY/MM/DD'),'10:00-11:00',?);
+
+insert into product_time(pt_no,pt_date, pt_time, p_no)
+ values(PRODUCT_TIME_PT_NO_SEQ.nextval,to_date('2021/10/22','YYYY/MM/DD'),'11:00-12:00',?);
+ 
+insert into product_time(pt_no,pt_date, pt_time, p_no) 
+values(PRODUCT_TIME_PT_NO_SEQ.nextval,to_date('2021/10/22','YYYY/MM/DD'),'13:00-14:00',?);
+*/
 
 /********** Cart **********/
 
@@ -34,8 +52,17 @@ insert into teacher(t_id,t_name,t_pass,t_email,t_photo,t_detail,t_location,p_no)
 
 /********** Reservation **********/
 /*
- insert into reservation(rsv_no, rsv_total, rsv_type, rsv_qty, rsv_date, rsv_time, rsv_paydate, m_no, p_no)
-values(RESERVATION_RSV_NO_SEQ.nextval,30000,'네이버페이',1,to_date('2021/10/21','YYYY/MM/DD'),'10:00-11:00',sysdate,1,1);
+ insert into reservation(rsv_no, rsv_total, rsv_type, rsv_qty, rsv_date, rsv_time, rsv_paydate, m_id, p_no)
+values(RESERVATION_RSV_NO_SEQ.nextval,30000,'네이버페이',1,to_date('2021/10/21','YYYY/MM/DD'),'10:00-11:00',sysdate,m_id,p_no);
+
+ insert into reservation(rsv_no, rsv_total, rsv_type, rsv_qty, rsv_date, rsv_time, rsv_paydate, m_id, p_no)
+values(RESERVATION_RSV_NO_SEQ.nextval,40000,'카카오페이',2,to_date('2021/10/22','YYYY/MM/DD'),'11:00-12:00',sysdate,m_id,p_no);
+
+ insert into reservation(rsv_no, rsv_total, rsv_type, rsv_qty, rsv_date, rsv_time, rsv_paydate, m_id, p_no)
+values(RESERVATION_RSV_NO_SEQ.nextval,30000,'네이버페이',1,to_date('2021/10/22','YYYY/MM/DD'),'10:00-11:00',sysdate,m_id,p_no);
+
+ insert into reservation(rsv_no, rsv_total, rsv_type, rsv_qty, rsv_date, rsv_time, rsv_paydate, m_id, p_no)
+values(RESERVATION_RSV_NO_SEQ.nextval,40000,'yoridogam페이',1,to_date('2021/10/21','YYYY/MM/DD'),'13:00-14:00',sysdate,m_id,p_no);
 */
 
 /********** Review **********/
