6eaadc80b3653e7bab8507ae416dfffde9a9a304
rlgnsgh123 <rlgnsgh123@gmail.com>
2021-10-25 17:28:01 +0900

    t_phone 추가

---
+++ src/main/java/com/itwill/yoridogam/controller/TeacherController.java
@@ -0,0 +1,5 @@
+package com.itwill.yoridogam.controller;
+
+public class TeacherController {
+
+}
--- src/main/java/com/itwill/yoridogam/teacher/teacher.sql
+++ src/main/java/com/itwill/yoridogam/teacher/teacher.sql
@@ -21,9 +21,9 @@ select count(*) cnt from teacher where m_id='teaher1';
     teacher_insert
     회원존재여부확인(회원가입시)
 */
-insert into teacher(t_id,t_name,t_pass,t_email,t_photo,t_detail,t_location,p_no) values('teacher1','kim1','1111','kkiho@naver.com','이미지1','일타강사','서울',1);
-insert into teacher(t_id,t_name,t_pass,t_email,t_photo,t_detail,t_location,p_no) values('teacher2','him1','2222','kkiho1@naver.com','이미지2','일타강사1','서울1',2);
-insert into teacher(t_id,t_name,t_pass,t_email,t_photo,t_detail,t_location,p_no) values('teacher3','tim1','3333','kkiho2@naver.com','이미지3','일타강사2','서울2',3);
+insert into teacher(t_id,t_name,t_pass,t_phone,t_email,t_photo,t_detail,t_location,p_no) values('teacher1','kim1','1111','010-0000-0000','kkiho@naver.com','이미지1','일타강사','서울',1);
+insert into teacher(t_id,t_name,t_pass,t_phone,t_email,t_photo,t_detail,t_location,p_no) values('teacher2','him1','2222','010-1111-1111','kkiho1@naver.com','이미지2','일타강사1','서울1',2);
+insert into teacher(t_id,t_name,t_pass,t_phone,t_email,t_photo,t_detail,t_location,p_no) values('teacher3','tim1','3333','010-2222-2222','kkiho2@naver.com','이미지3','일타강사2','서울2',3);
 
 
 
@@ -31,7 +31,7 @@ insert into teacher(t_id,t_name,t_pass,t_email,t_photo,t_detail,t_location,p_no)
     teacher_update
     회원수정
 */
-update teacher set t_pass='2222',t_email='dfff@naver.com',t_photo='이미지이1',t_detail='이타강사',t_location='인천',p_no='1' where t_id='teacher1';
+update teacher set t_pass='2222',t_phone='010-9999-9999',t_email='dfff@naver.com',t_photo='이미지이1',t_detail='이타강사',t_location='인천',p_no='1' where t_id='teacher1';
 
 /*
     teacher_remove
