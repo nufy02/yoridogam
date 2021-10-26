dbd9f0452a3d594d0bab443004f12ba19b6f8c54
eogus8292 <eogus8292@naver.com>
2021-10-26 17:19:48 +0900

    product에 teacher추가

--- src/main/java/com/itwill/yoridogam/product/test/ProductDaoTestMain.java
+++ src/main/java/com/itwill/yoridogam/product/test/ProductDaoTestMain.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.itwill.yoridogam.product.Product;
 import com.itwill.yoridogam.product.ProductDao;
+import com.itwill.yoridogam.teacher.Teacher;
 
 public class ProductDaoTestMain {
 
@@ -15,11 +16,13 @@ public class ProductDaoTestMain {
 		System.out.println(productDao);
 		
 		System.out.println("1. remove : "+productDao.deleteByNo(4));
-		System.out.println("2. create : "+productDao.create(new Product(0, "1r", "detail", "1", "1", "1", "1")));
-		System.out.println("3. selectall"+productDao.selectAll());
-		System.out.println("4. selectByno"+productDao.selectByNo(1));
-		Product p1 = new Product(5, "update", "update","update","update", "update", "update");
-		System.out.println("3. update :"+productDao.updateByNo(p1));
+		//System.out.println("2. create : "+productDao.create(new Product(0, "1r", "detail", "1", "1", "1", "1",new Teacher("teacher3", null, null, null, null, null, null, null, null))));
+		
+		//System.out.println("3. selectall"+productDao.selectAll());
+		//System.out.println("4. selectByno"+productDao.selectByNo(1));
+		//Product p1 = new Product(4, "update", "update","update","update", "update", "update",new Teacher("teacher3", null, null, null, null, null, null, null, null));
+		//System.out.println("3. update :"+productDao.updateByNo(p1));
+		
 	
 	}
 
