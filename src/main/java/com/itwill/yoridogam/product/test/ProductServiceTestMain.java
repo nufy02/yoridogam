337fc3cea123f7234924b4b8cf383c722d1196c4
eogus8292 <eogus8292@naver.com>
2021-10-26 17:22:30 +0900

    product에 teacher 추가

--- src/main/java/com/itwill/yoridogam/product/test/ProductServiceTestMain.java
+++ src/main/java/com/itwill/yoridogam/product/test/ProductServiceTestMain.java
@@ -6,6 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.itwill.yoridogam.product.Product;
 import com.itwill.yoridogam.product.ProductDao;
 import com.itwill.yoridogam.product.ProductService;
+import com.itwill.yoridogam.teacher.Teacher;
 
 public class ProductServiceTestMain {
 
@@ -16,10 +17,11 @@ public class ProductServiceTestMain {
 		System.out.println(productService);
 		
 		System.out.println("1. remove : "+productService.deleteByNo(4));
-		System.out.println("2. create : "+productService.create(new Product(0, "1r", "detail", "1", "1", "1", "1")));
+		System.out.println("2. create : "+productService.create(new Product(0, "1r", "detail", "1", "1", "1", "1",new Teacher("teacher3", null, null, null, null, null, null, null, null))));
+		
 		System.out.println("3. selectall"+productService.selectAll());
 		System.out.println("4. selectByno"+productService.selectByNo(1));
-		Product p1 = new Product(5, "update", "update","update","update", "update", "update");
+		Product p1 = new Product(4, "update", "update","update","update", "update", "update",new Teacher("teacher3", null, null, null, null, null, null, null, null));
 		System.out.println("5. update :"+productService.updateByNo(p1));
 	}
 
