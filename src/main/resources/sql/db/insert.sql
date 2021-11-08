fc7c24ce7056420ee719b00a7f81001e1f27934f
전하라 <doit160493@gmail.com>
2021-11-08 14:06:23 +0900

    ..

--- src/main/resources/sql/db/insert.sql
+++ src/main/resources/sql/db/insert.sql
@@ -141,7 +141,7 @@ insert into review values(REVIEW_R_NO_SEQ.nextval,'','너무 어려워요ㅠㅠ'
 insert into review values(REVIEW_R_NO_SEQ.nextval,'','이걸 돈받고 가르칠 생각을 하다니',1,'',sysdate,'member3',1);
 
 /********** Notice  엑셀 작업 **********/
-insert INTO NOTICE VALUES(1, '[yoridogam 전체 공지]저희 yoridogam이 온라인 강의를 시작하였습니다.', '2021-08-01', 20, 
+insert INTO NOTICE VALUES(notice_noti_no_SEQ.nextval, '[yoridogam 전체 공지]저희 yoridogam이 온라인 강의를 시작하였습니다.', '2021-08-01', 20, 
 '코로나 시기를 맞이하여 대면 수업에 어려움이 많으시죠?
 
 저희 요리도감에서는 온, 오프라인 강의를 모두 진행합니다.
@@ -149,7 +149,7 @@ insert INTO NOTICE VALUES(1, '[yoridogam 전체 공지]저희 yoridogam이 온�
 여러분의 많은 참여 부탁드립니다.
 
 감사합니다. ❤', 'admin');
-insert INTO NOTICE VALUES(2, '[yoridogam 전체 공지] 개인정보 처리방침 개정안내', '2021-08-03', 6, 
+insert INTO NOTICE VALUES(notice_noti_no_SEQ.nextval, '[yoridogam 전체 공지] 개인정보 처리방침 개정안내', '2021-08-03', 6, 
 '안녕하세요. yoridogam(이하 ‘회사’)입니다.
 
 서비스를 이용해 주시는 이용자 여러분께 감사드리며, 새로운 개인정보 처리방침 적용에 관한 안내 말씀 드립니다
@@ -157,7 +157,7 @@ insert INTO NOTICE VALUES(2, '[yoridogam 전체 공지] 개인정보 처리방�
 회사는 이용자 여러분의 개인정보를 무엇보다 소중하게 처리하고 있으며, 어떤 사안보다도 우선하여 안전하게 관리하고 있습니다.
 
 새롭게 변경될 개인정보 처리방침 내용을 확인하시고 서비스 이용에 참고하시기 바랍니다.', 'admin');
-insert INTO NOTICE VALUES(3, '[yoridogam 전체 공지] 강사 모집 공고', '2021-08-04', 8, 
+insert INTO NOTICE VALUES(notice_noti_no_SEQ.nextval, '[yoridogam 전체 공지] 강사 모집 공고', '2021-08-04', 8, 
 '안녕하세요. yoridogam(이하 ‘회사’)입니다.
 
 저희와 함께하실 강사님을 모집합니다.
@@ -167,7 +167,7 @@ insert INTO NOTICE VALUES(3, '[yoridogam 전체 공지] 강사 모집 공고', '
 참신하고 실력 좋은 강사님을 기다리겠습니다.
 
 감사합니다.', 'admin');
-insert INTO NOTICE VALUES(4, '[오프라인 클래스] 오프라인 클래스 개설 안내', '2021-09-07', 36, 
+insert INTO NOTICE VALUES(notice_noti_no_SEQ.nextval, '[오프라인 클래스] 오프라인 클래스 개설 안내', '2021-09-07', 36, 
 '안녕하세요, 요리도감을 사랑해 주시는 고객여러분.
 
 코로나로 집합 4인 이상 집합 금지로 중단되었던 오프라인 클래스가,
@@ -177,7 +177,7 @@ insert INTO NOTICE VALUES(4, '[오프라인 클래스] 오프라인 클래스 �
 강사님 포함 총 6분인지라 한 클래스에 총 5분의 수강생을 모집합니다. 
 
 많은 참여 부탁드립니다. 감사합니다. ', 'admin');
-insert INTO NOTICE VALUES(5, '[온라인 클래스] 아빠와 함께하는 강좌 이벤트', '2021-09-18', 36, 
+insert INTO NOTICE VALUES(notice_noti_no_SEQ.nextval, '[온라인 클래스] 아빠와 함께하는 강좌 이벤트', '2021-09-18', 36, 
 '안녕하세요. yoridogam입니다.
 
 아빠와 자식이 함께하는 이벤트 강좌가 개설되었습니다. 기간 내에만 열리는 강의로 기간 참고하시어 참여해 주시기 바랍니다. 
@@ -189,7 +189,7 @@ insert INTO NOTICE VALUES(5, '[온라인 클래스] 아빠와 함께하는 강�
 많은 참여와 관심 부탁드립니다.
 
 감사합니다.', 'admin');
-insert INTO NOTICE VALUES(6, '[yoridogam 전체 공지] yoridogam의 아름다운 나눔 이벤트', '2021-09-27', 100, 
+insert INTO NOTICE VALUES(notice_noti_no_SEQ.nextval, '[yoridogam 전체 공지] yoridogam의 아름다운 나눔 이벤트', '2021-09-27', 100, 
 '안녕하세요. yoridogam입니다.
 
 저희 yoridogam에서는 연말을 맞이하여 주변 온정이 필요한 이웃에게 전하는 요리 나눔 클래스를 개최할 예정입니다.
@@ -199,7 +199,7 @@ insert INTO NOTICE VALUES(6, '[yoridogam 전체 공지] yoridogam의 아름다�
 마음이 따듯하고 요리에 관심이 있는 누구라도 참여할 수 있습니다. 
 
 많은 관심과 참여 부탁드립니다. 감사합니다.', 'admin');
-insert INTO NOTICE VALUES(7, '[yoridogam 전체 공지] 결제 방식의 다양화가 이루어졌습니다.', '2021-10-01', 16, 
+insert INTO NOTICE VALUES(notice_noti_no_SEQ.nextval, '[yoridogam 전체 공지] 결제 방식의 다양화가 이루어졌습니다.', '2021-10-01', 16, 
 '안녕하세요, 요리도감을 사랑해 주시는 고객여러분.
 
 이전의 카드 결제, 무통장 입금으로만 진행되었던 결제 방식이
@@ -207,13 +207,13 @@ insert INTO NOTICE VALUES(7, '[yoridogam 전체 공지] 결제 방식의 다양�
 카카오페이가 추가되어 총 세 가지 방법으로 진행될 예정입니다.
 
 많은 참여 부탁드립니다. 감사합니다.', 'admin');
-insert INTO NOTICE VALUES(8, '[온/오프 통합] 2021년 11월 강의가 개설되었습니다.', '2021-10-30', 38, 
+insert INTO NOTICE VALUES(notice_noti_no_SEQ.nextval, '[온/오프 통합] 2021년 11월 강의가 개설되었습니다.', '2021-10-30', 38, 
 '안녕하세요, 요리도감을 사랑해 주시는 고객여러분.
 
 요리도감의 11월 강의는 강의 목록에서 확인하실 수 있습니다. 
 
 많은 관심 부탁드립니다.', 'admin');
-insert INTO NOTICE VALUES(9, '[온라인 클래스] 2+1 이벤트!', '2021-11-01', 8, 
+insert INTO NOTICE VALUES(notice_noti_no_SEQ.nextval, '[온라인 클래스] 2+1 이벤트!', '2021-11-01', 8, 
 '안녕하세요. yoridogam입니다.
 
 온라인 클래스에 많은 사랑 주시고 계셔서 이벤트를 진행해 보자 합니다.
@@ -225,7 +225,7 @@ insert INTO NOTICE VALUES(9, '[온라인 클래스] 2+1 이벤트!', '2021-11-01
 많은 참여와 관심 부탁드립니다.
 
 감사합니다.', 'admin');
-insert INTO NOTICE VALUES(10, '[온라인 클래스] 2+1 이벤트!', '2021-11-03', 8, '안녕하세요. yoridogam입니다.
+insert INTO NOTICE VALUES(notice_noti_no_SEQ.nextval, '[온/오프라인 클래스] 행복한 빼빼로데이 이벤트!', '2021-11-03', 8, '안녕하세요. yoridogam입니다.
 
 곧 다가올 즐거운 빼빼로데이를 위한 이벤트가 시작됩니다.
 
@@ -243,19 +243,19 @@ insert INTO NOTICE VALUES(10, '[온라인 클래스] 2+1 이벤트!', '2021-11-0
 /********** Inquiry 엑셀 작업 **********/ 
 
 insert into inquiry	
-values(1, '제가 1년 전쯤?? 들은 강의를 확인할 수 있나요??','확인하고 싶은 강의가 있는데 찾기가 어려워서 문의글 씁니다..','2021-08-15',
-35,0,1,0,'member1');
+values(inquiry_ib_no_SEQ.nextval, '제가 1년 전쯤?? 들은 강의를 확인할 수 있나요??','확인하고 싶은 강의가 있는데 찾기가 어려워서 문의글 씁니다..','2021-08-15',
+35,0,inquiry_ib_no_SEQ.nextval,0,'member1');
 
 insert into inquiry	
-values(2, '수업 후 만든 빵은','제가 가지고 가도 되는 거예요?','2021-08-29',
-3,0,2,0,'member1');
+values(inquiry_ib_no_SEQ.nextval, '수업 후 만든 빵은','제가 가지고 가도 되는 거예요?','2021-08-29',
+3,0,inquiry_ib_no_SEQ.nextval,0,'member1');
 
 insert into inquiry	
-values(3, '강의 비용에 재료값이 다 들어가져 있나요?','아니면 현장에서 더 결제를 해야 하는 건가요??','2021-09-03',
-43,	0,	3,	0,	'member1');
+values(inquiry_ib_no_SEQ.nextval, '강의 비용에 재료값이 다 들어가져 있나요?','아니면 현장에서 더 결제를 해야 하는 건가요??','2021-09-03',
+43,	0,	inquiry_ib_no_SEQ.nextval,	0,	'member1');
 
 insert into inquiry	
-values(4, '안녕하세요 고객님.^^ yoridogam입니다.','안녕하세요 고객님.^^ yoridogam입니다.
+values(inquiry_ib_no_SEQ.nextval, '안녕하세요 고객님.^^ yoridogam입니다.','안녕하세요 고객님.^^ yoridogam입니다.
 
 문의 주신 사항은 마이페이지 > 예약 내역 에서 조회 가능하십니다. 
 
@@ -266,19 +266,19 @@ Yoridogam 고객센터 02)1223-6678
 감사합니다.', '2021-08-16', 24,	1,	1,	1,	'admin');
 
 insert into inquiry	
-values(5, '원하는 수업 종류를 신청할 수 있나요?',	'저는 한식도 하고 싶고 간단한 일식도 하고 싶은데 없는 경우가 있더라구요!
-더 다양하게 있으면 좋을 것 같아서요..',	'2021-09-14', 23,	0,	5,	0,	'member2');
+values(inquiry_ib_no_SEQ.nextval, '원하는 수업 종류를 신청할 수 있나요?',	'저는 한식도 하고 싶고 간단한 일식도 하고 싶은데 없는 경우가 있더라구요!
+더 다양하게 있으면 좋을 것 같아서요..',	'2021-09-14', 23,	0,	inquiry_ib_no_SEQ.nextval,	0,	'member2');
 
 insert into inquiry	
-values(6, '코로나 때문에 신청한 강의가..', 	'취소되면 환불 일정은 어떻게 되는 거예요?','2021-09-20',	
-35,	0,	6,	0,	'member1');
+values(inquiry_ib_no_SEQ.nextval, '코로나 때문에 신청한 강의가..', 	'취소되면 환불 일정은 어떻게 되는 거예요?','2021-09-20',	
+35,	0,	inquiry_ib_no_SEQ.nextval,	0,	'member1');
 
 insert into inquiry	
-values(7, '카카오페이로 결제 되니까 앞으로',	'네이버페이도 곧 들어오게 될 수 있을까요??','2021-10-11',
-12,	0,	7,	0,	'member3');
+values(inquiry_ib_no_SEQ.nextval, '카카오페이로 결제 되니까 앞으로',	'네이버페이도 곧 들어오게 될 수 있을까요??','2021-10-11',
+12,	0,	inquiry_ib_no_SEQ.nextval,	0,	'member3');
 
 insert into inquiry	
-values(8, '안녕하세요 고객님.^^ yoridogam입니다.',	'안녕하세요 고객님.^^ yoridogam입니다.
+values(inquiry_ib_no_SEQ.nextval, '안녕하세요 고객님.^^ yoridogam입니다.',	'안녕하세요 고객님.^^ yoridogam입니다.
 
 만드신 음식 및 빵류는 만드신 분이 가져가는 것을 원칙으로 하고 있습니다.
 
@@ -289,15 +289,15 @@ Yoridogam 고객센터 02)1223-6678
 감사합니다.',	'2021-08-30',	18, 1, 2,	1,	'admin');
 
 insert into inquiry	
-values(9, '예전에 아빠와 함께 이벤트',	'당첨됐다고 문자가 온 후로 수강권을 못 받은 것 같아서요 어떻게 해요?',
-'2021-11-01',	7,	0,	9,	0,	'member1');
+values(inquiry_ib_no_SEQ.nextval, '예전에 아빠와 함께 이벤트',	'당첨됐다고 문자가 온 후로 수강권을 못 받은 것 같아서요 어떻게 해요?',
+'2021-11-01',	7,	0,	inquiry_ib_no_SEQ.nextval,	0,	'member1');
 
 insert into inquiry	
-values(10, '제가 진짜 요.알.못인데 수업 따라갈수 있을까요?',	'ㅠㅠ 혹시 뒤쳐지면 민폐잖아용',	'2021-11-01',	
-3,	0,	10,	0,	'member1');
+values(inquiry_ib_no_SEQ.nextval, '제가 진짜 요.알.못인데 수업 따라갈수 있을까요?',	'ㅠㅠ 혹시 뒤쳐지면 민폐잖아용',	'2021-11-01',	
+3,	0,	inquiry_ib_no_SEQ.nextval,	0,	'member1');
 
 insert into inquiry	
-values(11,'안녕하세요 고객님.^^ yoridogam입니다.', '"안녕하세요 고객님.^^ yoridogam입니다.
+values(inquiry_ib_no_SEQ.nextval,'안녕하세요 고객님.^^ yoridogam입니다.', '"안녕하세요 고객님.^^ yoridogam입니다.
 
 저희 수강료에는 재료값이 포함되어 있어 현장에서 추가로 결제하실 것은 없습니다. 
 
@@ -308,11 +308,11 @@ Yoridogam 고객센터 02)1223-6678
 감사합니다.',	'2021-09-04',	10,	1,	3,	1,	'admin');
 
 insert into inquiry	
-values(12, '온라인+오프라인 하나씩 해도', '2+1 이벤트 자동 응모인 거예요? 아니면 온라인만인 거예요??', '2021-11-02',
-27,	0,	12,	0,	'member1');
+values(inquiry_ib_no_SEQ.nextval, '온라인+오프라인 하나씩 해도', '2+1 이벤트 자동 응모인 거예요? 아니면 온라인만인 거예요??', '2021-11-02',
+27,	0,	inquiry_ib_no_SEQ.nextval,	0,	'member1');
 
 insert into inquiry	
-values(13, '안녕하세요 고객님.^^ yoridogam입니다.',	'안녕하세요 고객님.^^ yoridogam입니다.
+values(inquiry_ib_no_SEQ.nextval, '안녕하세요 고객님.^^ yoridogam입니다.',	'안녕하세요 고객님.^^ yoridogam입니다.
 
 먼저 소중한 의견 감사합니다. 문의게시판에 작성해 주시면 저희가 검토 후 순차적으로 개설해 보겠습니다.
 
@@ -323,11 +323,11 @@ Yoridogam 고객센터 02)1223-6678
 감사합니다.',	'2021-09-15',	19,	1,	5,	1,	'admin');
 
 insert into inquiry	
-values(14, '빼빼로데이 이벤트 말이에요', '온라인/오프라인 각각 하나씩 결제해도 해당사항 있는 건가요?', 	
-'2021-11-05', 	9,	0,	14,	0,	'member1');
+values(inquiry_ib_no_SEQ.nextval, '빼빼로데이 이벤트 말이에요', '온라인/오프라인 각각 하나씩 결제해도 해당사항 있는 건가요?', 	
+'2021-11-05', 	9,	0,	inquiry_ib_no_SEQ.nextval,	0,	'member1');
 
 insert into inquiry	
-values(15, '안녕하세요 고객님.^^ yoridogam입니다.',	'안녕하세요 고객님.^^ yoridogam입니다.
+values(inquiry_ib_no_SEQ.nextval, '안녕하세요 고객님.^^ yoridogam입니다.',	'안녕하세요 고객님.^^ yoridogam입니다.
 
 강사님 일정에 따라 연장 또는 환불 진행될 예정이며, 
 온라인 강의는 해당 사항 없습니다.
@@ -339,7 +339,7 @@ Yoridogam 고객센터 02)1223-6678
 감사합니다.',	'2021-09-23',	14,	1,	6,	1,	'admin');
 
 insert into inquiry	
-values(16, '안녕하세요 고객님.^^ yoridogam입니다.',	'안녕하세요 고객님.^^ yoridogam입니다.
+values(inquiry_ib_no_SEQ.nextval, '안녕하세요 고객님.^^ yoridogam입니다.',	'안녕하세요 고객님.^^ yoridogam입니다.
 
 아직 일정은 없으나, 검토해 보겠습니다.
 
